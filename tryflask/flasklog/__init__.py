68877c277eabc82611c252cedabeb4061adb4424
hox294 <hong.xue@usask.ca>
2019-08-02 11:01:02 -0600

    add forget password function

--- tryflask/flasklog/__init__.py
+++ tryflask/flasklog/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 
 app = Flask(__name__)
@@ -13,5 +15,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "hongyi.cmpt@gmail.com"
+app.config['MAIL_PASSWORD'] = "19951212dtm"
+mail = Mail(app)
 
 from flasklog import routes
--- tryflask/flasklog/routes.py
+++ tryflask/flasklog/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, url_for, flash, redirect, request, abort
-from flasklog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
-from flasklog import app, db, bcrypt
+from flasklog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
+from flasklog import app, db, bcrypt, mail
 from flasklog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 @app.route('/')
 @app.route('/home')
@@ -114,3 +115,53 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+# @app.route("/user/<str:username>")
+# def user_cart():
+#     page = request
+#     posts = Post.query.all()
+#     return render_template('home.html', posts = posts)
+
+def send_reset_email(user: User):
+    token = user.get_reset_token()
+    msg = Message('Passwod Reset Request', sender='hongyi.cmpt@gmail.com', recipients=[user.email])
+
+    msg.body = f'''To reset your password, visit the link below:
+{url_for('reset_token', token=token, _external=True)}
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))    #make sure user log out 
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user:
+            send_reset_email(user)
+            flash('An email has been sent with instruction to reset your password', 'info')
+            return redirect(url_for('login'))
+        else:
+            flash('There is no account under this email address', 'danger')
+    return render_template('reset_request.html', title = 'Reset Password', form = form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))    
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invaild or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been reseted! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('reset_token.html', title = 'Reset Password', form = form)
